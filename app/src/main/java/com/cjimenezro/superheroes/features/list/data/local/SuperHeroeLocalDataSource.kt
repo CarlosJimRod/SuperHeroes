37121f7f0af5e18cc8a42fb5c60417867aeef0e7
Cjr37 <carlosclase37@gmail.com>
2023-10-25 02:22:45 +0200

    Creados LocalDataSource para las tres clases que lo requieren

--- app/src/main/java/com/cjimenezro/superheroes/features/list/data/local/SuperHeroeLocalDataSource.kt
+++ app/src/main/java/com/cjimenezro/superheroes/features/list/data/local/SuperHeroeLocalDataSource.kt
@@ -1,11 +1,46 @@
 package com.cjimenezro.superheroes.features.list.data.local
 
 import android.content.Context
+import com.cjimenezro.superheroes.app.Either
+import com.cjimenezro.superheroes.app.ErrorApp
+import com.cjimenezro.superheroes.app.left
+import com.cjimenezro.superheroes.app.right
 import com.cjimenezro.superheroes.app.serialization.JsonSerialization
+import com.cjimenezro.superheroes.features.list.data.remote.SuperHeroePincipalDataApiModel
+import com.cjimenezro.superheroes.features.list.domain.SuperHeroePrincipalData
+import com.cjimenezro.superheroes.features.list.domain.SuperHeroeWork
 
 class SuperHeroeLocalDataSource(private val context: Context,private val jsonSerialization: JsonSerialization) {
 
     private val sharedPref=context.getSharedPreferences("SuperHeroes",Context.MODE_PRIVATE)
+
+    fun saveSuperHeroe(superHeroe: SuperHeroePrincipalData): Either<ErrorApp, Boolean> {
+        return try {
+            with(sharedPref.edit()) {
+                val jsonSuperHeroe = jsonSerialization.toJson(superHeroe, SuperHeroePrincipalData::class.java)
+                putString(superHeroe.id.toString(), jsonSuperHeroe)
+                apply()
+            }
+            true.right()
+        }catch (ex:Exception){
+            return ErrorApp.UnknowError.left()
+        }
+    }
+
+    fun getSuperHeroe(): Either<ErrorApp, List<SuperHeroePrincipalData>> {
+        return try {
+            val jsonSuperHeroe=sharedPref.all as Map<String,String>
+
+            val hotels=jsonSuperHeroe.values.map {
+                jsonSerialization.fromJson(it,SuperHeroePrincipalData::class.java)
+            }
+
+            return hotels.right()
+        }catch (ex:Exception){
+            return ErrorApp.UnknowError.left()
+        }
+
+    }
 
 
 
