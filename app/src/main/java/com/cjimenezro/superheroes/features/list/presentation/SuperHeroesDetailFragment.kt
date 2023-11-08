84cc5e0a64ce670de1614542aa86bd1b172e422c
Cjr37 <carlosclase37@gmail.com>
2023-11-08 02:58:58 +0100

    Implementado Navigation

--- app/src/main/java/com/cjimenezro/superheroes/features/list/presentation/SuperHeroesDetailFragment.kt
+++ app/src/main/java/com/cjimenezro/superheroes/features/list/presentation/SuperHeroesDetailFragment.kt
@@ -9,6 +9,7 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.cjimenezro.superheroes.R
 import com.cjimenezro.superheroes.app.ErrorApp
@@ -32,6 +33,8 @@ class SuperHeroesDetailFragment : Fragment() {
 
     private var _binding: FragmentSuperHeroeDetailsBinding? = null
     private val binding get() = _binding!!
+
+    val args: SuperHeroesDetailFragmentArgs by navArgs()
 
     val viewModel:SuperHeroesDetailViewModel by lazy {
         SuperHeroesDetailViewModel(
@@ -64,8 +67,6 @@ class SuperHeroesDetailFragment : Fragment() {
         setupView()
         return binding.root
     }
-
-    private fun getParams(): String = arguments?.getString("ARG_PARAM1") ?: ""
 
     private fun setupView(){
         binding.apply {
@@ -116,9 +117,8 @@ class SuperHeroesDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val heroId=getParams()
         setupObservers()
-        viewModel.loadSuperHeroe(heroId)
+        viewModel.loadSuperHeroe(args.subjectId)
     }
 
     private fun setupObservers(){
--- app/src/main/java/com/cjimenezro/superheroes/features/list/presentation/SuperHeroesListFragment.kt
+++ app/src/main/java/com/cjimenezro/superheroes/features/list/presentation/SuperHeroesListFragment.kt
@@ -1,13 +1,12 @@
 package com.cjimenezro.superheroes.features.list.presentation
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.cjimenezro.superheroes.R
 import com.cjimenezro.superheroes.app.serialization.GsonSerialization
@@ -65,10 +64,17 @@ class SuperHeroesListFragment : Fragment() {
                 false
             )
             superHeroApadter.setEvent {
-                (activity as MainActivity).changeFragment(SuperHeroesDetailFragment.newInstance(it.toString()))
+                findNavController().navigate(
+                    SuperHeroesListFragmentDirections.actionFromFragmentListToFragmentDetail(it.toString())
+                )
             }
             list.adapter = superHeroApadter
             layoutList.toolbar.apply {
+
+                setNavigationOnClickListener{
+                    findNavController().navigateUp()
+                }
+
                 //Establecemos el título por programación
                 title = getString(R.string.fragment_list_title)
 
